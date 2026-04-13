11986fc420c9bf319c82a7cf17ac600114b50339
kalharaweragoda <kalharaweragoda@gmail.com>
2026-04-13 20:06:22 +0530

    create ScrollView

--- App.tsx
+++ App.tsx
@@ -1,15 +1,16 @@
 import React from "react";
-import { Button,  View, StyleSheet } from "react-native";
+import { Button,  View, StyleSheet, Text, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function SignUpScreen() {
   return (
     
-      <SafeAreaView>
+      <ScrollView>
         <View>
-          <button title="Java Institute"/>
+          <Text>Create Account</Text>
+          <Text>Fill in the information below to create your account.</Text>
         </View>
-      </SafeAreaView>
+      </ScrollView>
       
   );
 }
